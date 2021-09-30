f88cf0c75a03fdac507121b740689c08819658fb
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-09-30 14:17:34 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,36 @@
+//A
+/*const superheroes = ([   
+    { name: "Batman", alter_ego: "Bruce Wayne" },
+    { name: "Superman", alter_ego: "Clark Kent" },
+    { name: "Spiderman", alter_ego: "Peter Parker" }
+]);
+const searchedItem = superheroes.find(findSpiderMan);
+
+// Hier komt jouw functie 
+function findSpiderMan(Element){
+    return Element.name == "Spiderman";
+};
+console.log(searchedItem);*/
+ // Find Spiderman
+// result should be: {name: "Spiderman", alter_ego: "Peter Parker"
+
+//B
+/*const doubleNumbers = [1,2,3];
+
+const doubleArrayNumbers =  doubleNumbers(findNumbers);
+
+function findNumbers(doubleNumber,index) {
+    return Element.doubleNumbers.forEach(element => {
+         }
+});
+
+consolo.log (doubleArrayValues([1, 2, 3]); 
+  // result should be [2, 4, 6]*/
+
+const doubleArrayValues = [1, 2, 3,];
+let sum = 0;
+
+ doubleArrayValues.forEach(item => {
+     sum += item;
+     console.log(sum)
+ });  
